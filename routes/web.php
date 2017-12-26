bbbca123bc1c606f9b1eddddb04409b61b906b7f
ng <1062877403@qq.com>
2017-12-26 13:58:17 +0800

    setup default home page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@
 /*Route::get('/', function () {
     return view('welcome');
 });*/
-Route::get('/','PagesController@root')->name('root');
-
+//Route::get('/','PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 //Auth::routes();
  // Authentication Routes...
  Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
